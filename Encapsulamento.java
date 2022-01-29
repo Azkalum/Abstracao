529c38488a0afa9cf744fa552eb148d94b3b423b
Renan Almeida <98130954+Azkalum@users.noreply.github.com>
2022-01-29 13:43:46 -0300

    Add files via upload

---
+++ Encapsulamento.java
@@ -0,0 +1,29 @@
+package encapsulamento;
+
+public class Encapsulamento {
+    
+    public static void main(String args[]) {
+        
+    }
+    private double salario;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+} 
+        
+    
+    
